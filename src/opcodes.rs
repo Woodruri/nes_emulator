6b6b9c4cfa5c26508459324d6b60ca8c8874d24d
Ruslan Vudraff <woodruff.l.riley@gmail.com>
2026-01-08 11:35:01 -0800

    fixed opcodes impl

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -21,46 +21,53 @@ impl OpCode {
         }
     }
 
-}
+    lazy_static! {
+        public static ref CPU_OP_CODES:Vec<OpCode> vec![
 
-lazy_static! {
-    public static ref CPU_OP_CODES:Vec<OpCode> vec![
-
-        //========================= BRK ===================================
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
+            //========================= BRK ===================================
+            OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
 
 
-        //========================= TAX ===================================
-        OpCode::new(0xaa, "TAX", 1, 2, AddressingMode::NoneAddressing),
+            //========================= TAX ===================================
+            OpCode::new(0xaa, "TAX", 1, 2, AddressingMode::NoneAddressing),
 
 
-        //========================= INX ===================================
-        OpCode::new(0xe8, "INX", 1, 2, AddressingMode::NoneAddressing),
+            //========================= INX ===================================
+            OpCode::new(0xe8, "INX", 1, 2, AddressingMode::NoneAddressing),
 
-        //========================= LDA ===================================
-        OpCode::new(0xa9, "LDA", 2, 2, AddressingMode::Immediate),
+            //========================= LDA ===================================
+            OpCode::new(0xa9, "LDA", 2, 2, AddressingMode::Immediate),
 
-        OpCode::new(0xa5, "LDA", 2, 3, AddressingMode::ZeroPage),
-        OpCode::new(0xb5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
+            OpCode::new(0xa5, "LDA", 2, 3, AddressingMode::ZeroPage),
+            OpCode::new(0xb5, "LDA", 2, 4, AddressingMode::ZeroPage_X),
 
-        OpCode::new(0xad, "LDA", 3, 4, AddressingMode::Absolute),
-        OpCode::new(0xbd, "LDA", 3, 4, AddressingMode::Absolute_X), //+1 if page crossed
-        OpCode::new(0xb9, "LDA", 3, 4, AddressingMode::Absolute_Y), //+1 if page crossed
+            OpCode::new(0xad, "LDA", 3, 4, AddressingMode::Absolute),
+            OpCode::new(0xbd, "LDA", 3, 4, AddressingMode::Absolute_X), //+1 if page crossed
+            OpCode::new(0xb9, "LDA", 3, 4, AddressingMode::Absolute_Y), //+1 if page crossed
 
-        OpCode::new(0xa1, "LDA", 2, 4, AddressingMode::Indirect_X),
-        OpCode::new(0xb1, "LDA", 2, 4, AddressingMode::Indirect_Y), //+1 if page crossed
+            OpCode::new(0xa1, "LDA", 2, 4, AddressingMode::Indirect_X),
+            OpCode::new(0xb1, "LDA", 2, 4, AddressingMode::Indirect_Y), //+1 if page crossed
 
-        //========================= STA ===================================
-        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
-        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
+            //========================= STA ===================================
+            OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+            OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
 
-        OpCode::new(0x8d, "STA", 2, 4, AddressingMode::Absolute),
-        OpCode::new(0x9d, "STA", 2, 5, AddressingMode::Absolute_X),
-        OpCode::new(0x99, "STA", 2, 5, AddressingMode::Absolute_Y),
+            OpCode::new(0x8d, "STA", 2, 4, AddressingMode::Absolute),
+            OpCode::new(0x9d, "STA", 2, 5, AddressingMode::Absolute_X),
+            OpCode::new(0x99, "STA", 2, 5, AddressingMode::Absolute_Y),
 
-        OpCode::new(0x81, "STA", 2, 6, AddressingMode::Indirect_X),
-        OpCode::new(0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
+            OpCode::new(0x81, "STA", 2, 6, AddressingMode::Indirect_X),
+            OpCode::new(0x91, "STA", 2, 6, AddressingMode::Indirect_Y),
 
 
-    ]
+        ];
+
+        public static ref OPCODES_MAP: HashMap<u8, &'static OpCode> = {
+            let mut map = HashMap::new();
+            for cpuop in &'CPU_OP_CODES {
+                map.insert(cpuop.code, cpuop)
+            }
+        }
+    }
+
 }
